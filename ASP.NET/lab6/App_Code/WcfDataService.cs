646ab97ce9b06f9ab79952ec975949ddd7abe8dd
Aleksandar7kr <Aleksandar7kr@gmail.com>
2012-11-04 17:04:25 +0300

    lab6 done

---
+++ ASP.NET/lab6/App_Code/WcfDataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Web;
+
+public class WcfDataService : DataService<NORTHWNDModel.NORTHWNDEntities>
+{
+    // Этот метод вызывается только один раз для инициализации серверных политик.
+    public static void InitializeService(DataServiceConfiguration config)
+    {
+        // TODO: задайте правила, чтобы указать, какие наборы сущностей и служебные операции являются видимыми, обновляемыми и т.д.
+        // Примеры:
+         config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
+        // config.SetServiceOperationAccessRule("СлужебнаяОперация", ServiceOperationRights.All);
+        config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+    }
+}
---
+++ ASP.NET/lab6_client/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab6_client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServiceReference1.NORTHWNDEntities proxy =
+                new ServiceReference1.NORTHWNDEntities
+                    (new Uri("http://localhost:53519/lab6/WcfDataService.svc/"));
+            this.customersBindingSource.DataSource = proxy.Customers;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.NORTHWNDEntities proxy =
+                new ServiceReference1.NORTHWNDEntities(
+                    new Uri("http://localhost:53519/lab6/WcfDataService.svc/"));
+            string city = textBox1.Text;
+
+            if (city != "")
+            {
+                this.customersBindingSource.DataSource = 
+                    from c in proxy.Customers where c.City == city select c;
+            }
+
+        }
+
+
+    }
+}
