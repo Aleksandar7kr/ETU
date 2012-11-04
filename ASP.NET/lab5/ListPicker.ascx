2b9af6e1223cfd6bad5f3f3f4e318a0e83b83d3d
Aleksandar7kr <Aleksandar7kr@gmail.com>
2012-11-04 17:19:51 +0300

    lab5. not ended, because description is corrupt

---
+++ ASP.NET/lab5/ListPicker.ascx
@@ -0,0 +1,47 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true"
+ CodeFile="ListPicker.ascx.cs" Inherits="ListPicker" %>
+
+
+<style type="text/css">
+    .style1
+    {
+        width: 39%;
+    }
+    .style2
+    {
+        width: 200px;
+    }
+    .style3
+    {
+        width: 31px;
+    }
+</style>
+
+
+
+<table class="style1">
+    <tr>
+        <td class="style2">
+            Доступно<br />
+            <asp:ListBox ID="sourceList" runat="server" Height="200px" Width="200px">
+            </asp:ListBox>
+        </td>
+        <td class="style3">
+            <asp:Button ID="addAll" runat="server" Text="&gt;&gt;" onclick="addAll_Click" 
+                Width="29px" />
+            <br />
+            <asp:Button ID="addOne" runat="server" onclick="addOne_Click" Text=" &gt; " 
+                Width="29px" />
+            <br />
+            <asp:Button ID="remove" runat="server" onclick="remove_Click" Text=" X " />
+        </td>
+        <td>
+            Выбрано<br />
+            <asp:ListBox ID="targetList" runat="server" Height="200px" Width="200px">
+            </asp:ListBox>
+        </td>
+    </tr>
+</table>
+
+
+
---
+++ ASP.NET/lab5/ListPicker.ascx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ListPicker : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void addAll_Click(object sender, EventArgs e)
+    {
+        targetList.SelectedIndex = -1;
+        foreach (ListItem li in sourceList.Items)
+        {
+            AddItem(li);
+        }
+    }
+    protected void addOne_Click(object sender, EventArgs e)
+    {
+        if (sourceList.SelectedIndex >= 0)
+        {
+            AddItem(sourceList.SelectedItem);
+        } 
+    }
+
+    protected void remove_Click(object sender, EventArgs e)
+    {
+        if (targetList.SelectedIndex >= 0)
+        {
+            targetList.Items.RemoveAt(targetList.SelectedIndex);
+            targetList.SelectedIndex = -1;
+        } 
+
+    }
+
+    protected void AddItem(ListItem li)
+    {
+        targetList.SelectedIndex = -1;
+        if (this.AllowDuplicates == true)
+        {
+            targetList.Items.Add(li);
+        }
+        else
+        {
+            if (targetList.Items.FindByText(li.Text) == null)
+            {
+                targetList.Items.Add(li);
+            }
+        }
+    }
+
+    public ListItemCollection SelectionItems
+    {
+        get { return targetList.Items; }
+    }
+
+    public bool AllowDuplicates
+    {
+        get 
+        {
+            return (bool)ViewState["allowDuplicates"];
+        }
+        set
+        {
+            ViewState["allowDuplicates"] = value;
+        }
+
+    }
+
+    public void AddSourceItem(String text)
+    {
+        sourceList.Items.Add(text);
+    }
+
+    public void ClearAll()
+    {
+        sourceList.Items.Clear();
+        targetList.Items.Clear();
+    }
+
+}
