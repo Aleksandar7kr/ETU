080fc294a50441b1b892340efc8710fda26ff223
Aleksandar7kr <Aleksandar7kr@gmail.com>
2012-12-10 20:45:52 +0300

    [Pattern] lab 1.3 Entity Framework done

---
+++ DesignPatterns/UseEntityFramework/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace UseEntityFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeView.FullRowSelect = true;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(305, 402);
+            this.treeView.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 401);
+            this.Controls.Add(this.treeView);
+            this.Name = "Form1";
+            this.Text = "Сотрудники";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+    }
+}
+
---
+++ DesignPatterns/UseEntityFramework/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UseEntityFramework
+{
+    public partial class Form1 : Form
+    {
+        private AdventureWorksEntities db;
+        
+        public Form1()
+        {
+            db = new AdventureWorksEntities();
+            InitializeComponent();
+            GetPredisentName();
+        }
+
+        private void GetPredisentName()
+        {
+            var p = db.Employee
+                .Where (o => o.ManagerID == null);
+
+            foreach (var emp in p)
+            {
+                AddEmployeeToHierarchy((int)emp.EmployeeID, treeView.Nodes.Add(emp.Contact.FirstName + " " + emp.Contact.LastName));
+            }
+
+        }
+
+        private void MakeHierarchy()
+        {
+                        
+        }
+
+        public void AddEmployeeToHierarchy(int EmpID, TreeNode node)
+        {
+            System.Data.Objects.ObjectResult<uspGetManagerEmployees_Result> res = db.uspGetManagerEmployees(EmpID);
+
+            foreach (var emp in res)
+            {
+                AddEmployeeToHierarchy((int)emp.EmployeeID, node.Nodes.Add(emp.FirstName + " " + emp.LastName));
+            }
+
+        }
+
+    }
+}
