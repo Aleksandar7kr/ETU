23a21efba102533b8ce18b70395b80606a2523ff
Aleksandar7kr <Aleksandar7kr@gmail.com>
2012-12-15 03:58:08 +0300

    [pattern done]

---
+++ DesignPatterns/OopLabs.Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace OopLabs.Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoSomething(LogToFile);
+            DoSomething(delegate(string message) { Console.WriteLine(message); });
+            DoSomething(message => Console.WriteLine(message));
+            DoSomething(Console.WriteLine);
+            Console.ReadKey();
+        }
+
+        static void DoSomething(Action<string> log)
+        {
+            log(DateTime.Now + " : DoSomething()");
+        }
+
+        static void LogToFile(string message)
+        {
+            string myDocsPath =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string logFilePath = Path.Combine(myDocsPath,"log.txt");
+            File.AppendAllText(logFilePath, message + Environment.NewLine);
+        }
+    }
+}
---
+++ DesignPatterns/OopLabs.Delegates/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("OopLabs.Delegates")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OopLabs.Delegates")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("23f2b507-d487-46bb-96f6-64ea3c5be62e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
